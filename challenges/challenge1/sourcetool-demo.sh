c4b8564423df43f2a8c3d1ee72d970092abdb22d
sherine-k <skhoury@redhat.com>
2026-06-21 11:44:47 +0200

    Updates and fixes : Intro and challenge1

--- challenges/challenge1/sourcetool-demo.sh
+++ challenges/challenge1/sourcetool-demo.sh
@@ -21,21 +21,20 @@ fi
 REPO="supply-chain-dd/supply-chain-dd"
 BRANCH="main"
 
-p "=== SLSA Source Tool - Vérification de la provenance du code source ==="
+# p "=== SLSA Source Tool - Vérification de la provenance du code source ==="
 
 
-p "1. Vérifier le statut du dépôt sur la branche $BRANCH"
+p "Vérifier le statut du dépôt sur la branche $BRANCH"
 pe "sourcetool status ${REPO}@${BRANCH}"
 
-p "2. Récupérer le dernier commit sur $BRANCH"
-pe "git fetch origin refs/notes/commits:refs/notes/commits"
-pe "COMMIT_ID=\$(git rev-parse origin/${BRANCH})"
+# p "2. Récupérer le dernier commit sur $BRANCH"
+git fetch origin refs/notes/commits:refs/notes/commits
 COMMIT_ID=$(git rev-parse origin/${BRANCH})
 
-p "3. Afficher l'attestation SLSA stockée dans les git notes"
-pe "git notes show ${COMMIT_ID} | jq"
-
-p "4. Vérifier la provenance du commit avec sourcetool"
+p "Vérifier la provenance du commit avec sourcetool"
 pe "sourcetool verifycommit ${REPO}@${BRANCH}"
+
+# p "3. Afficher l'attestation SLSA stockée dans les git notes"
+pe "git notes show ${COMMIT_ID} | jq"
 
 p "✅"
